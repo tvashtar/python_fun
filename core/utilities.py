8227fe72fe69b6b7213c6fbfe5b663a08c045b4b
Conor <conorlaver@gmail.com>
2017-12-10 14:22:36 -0500

    Refined summarize_dict

--- core/utilities.py
+++ core/utilities.py
@@ -1,25 +1,30 @@
-import pandas as pd
 
-def summarize_json(raw_json, max_expand=20):
-    for key in raw_json:
-        value = raw_json[key] 
-        if isinstance(value, (int, float, complex, str, tuple)):
-            print(f'{key}: {value}')
-        elif isinstance(value, list):
-            print(f'{key}: {type(value)} with {len(value)} elements')
-            if len(value) <  max_expand:
-                for item in value:
-                    if isinstance(value, (int, float, complex, str, tuple)):
-                        print(f'    {item}')
-                    else:
-                        print(f'    {type(item)} with {len(item)} elements')
-        elif isinstance(value, dict):
-            print(f'{key}: {type(value)} with {len(value)} elements')
-            if len(value.keys()) <  max_expand:
-                for item in value.keys():
-                    if isinstance(value[item], (int, float, complex, str, tuple)):
-                        print(f'    {item}: {value[item]}')
-                    else:
-                        print(f'    {item}: {type(value[item])} with {len(value[item])} elements')          
-        else:
-            print(f'{key}: {type(value)}')
+
+def summarize_dict(my_dict, max_expand=10, max_depth=3, key='Top Level', depth=0):
+    depth += 1
+    print('    '*depth + f'{key}: {type(my_dict)} with {len(my_dict)} elements')
+    if depth > max_depth:
+        return
+    if len(my_dict.keys()) <=  max_expand:
+        for item in my_dict.keys():
+            if isinstance(my_dict[item], (int, float, complex, str, tuple)):
+                print('    '*(depth+1) + f'{item}: {my_dict[item]}')
+            elif isinstance(my_dict[item], list):
+                summarize_list(my_dict[item], max_expand, max_depth, key=item, depth=depth)
+            elif isinstance(my_dict[item], dict):
+                summarize_dict(my_dict[item], max_expand, max_depth, key=item, depth=depth)
+                
+def summarize_list(my_list, max_expand, max_depth, key='Top Level', depth=0):
+    depth += 1
+    print('    '*depth + f'{key}: {type(my_list)} with {len(my_list)} elements')
+    if depth > max_depth:
+        return
+    if len(my_list) <=  max_expand:
+        for item in my_list:
+            if isinstance(item, (int, float, complex, str, tuple)):
+                print('    '*(depth+1) + f'{item}')
+            elif isinstance(item, list):
+                summarize_list(item, max_expand, max_depth, key='', depth=depth)
+            elif isinstance(item, dict):
+
+                summarize_dict(item, max_expand, max_depth, key='', depth=depth)
